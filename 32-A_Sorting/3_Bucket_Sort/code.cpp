9f58d2614e9a6219e22d877a2d51f85c37df6809
prashantthakur207 <pt264doc@gmail.com>
2024-11-18 00:52:58 +0530

    change to optimise

--- 32-A_Sorting/3_Bucket_Sort/code.cpp
+++ 32-A_Sorting/3_Bucket_Sort/code.cpp
@@ -7,10 +7,25 @@ void BucketSort(vector<float>& v){
     int n = v.size();  
     // step 1 to create bucket of size n
     vector<vector<float>> bucket(n,vector<float> ());
+    // finding range
+    float min_ele = v[0];
+    float max_ele = v[0];
+    for(int i=1; i<n; i++){
+        min_ele = min(min_ele,v[i]);
+        max_ele = max(max_ele,v[i]);
+    }
+    int range = (max_ele-min_ele)/n;
     // step 2 to insert ele into bucket acc to index
     for(int i=0; i<n; i++){
-         int index  = v[i]*n; 
-         bucket[index].push_back(v[i]);
+         int index  = (v[i]-min_ele)/range;
+         // boundary ele
+         float diff = (v[i]-min_ele)/range-index; 
+         if(diff==0 && v[i]!=min_ele){
+             bucket[index-1 ].push_back(v[i]);
+         }
+         else{
+             bucket[index].push_back(v[i]);
+         }
     }
     // step 3 sort individual bucket
     for(int i=0; i<n; i++){
